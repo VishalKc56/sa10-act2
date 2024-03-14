7a4f214da5831a56876c8cb40ca571112e293d7b
Vishal Chintakunta <vkchntkn@memphis.edu>
2024-03-14 18:55:39 -0500

    Reached green and added backstage_tick method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -69,7 +69,19 @@ class GildedRose
     @quality += 1 if @days_remaining <= 0
   end
 
-  def 
+  def sulfuras_tick
+  end
+
+  def backstage_tick
+    @days_remaining -= 1
+    return    if @quality >= 50
+    return @quality = 0 if @days_remaining <= 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+
+  end
 
 end
 
